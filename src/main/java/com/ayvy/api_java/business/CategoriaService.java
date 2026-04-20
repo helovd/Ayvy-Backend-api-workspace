e8b3b93de7aa76ba607678707864789d8e884d8b
helovd <heloisavidaldourado@outlook.com>
2026-04-19 23:20:04 -0300

    CRUD (sem o update) de categoria

--- src/main/java/com/ayvy/api_java/business/CategoriaService.java
+++ src/main/java/com/ayvy/api_java/business/CategoriaService.java
@@ -1,4 +1,39 @@
 package com.ayvy.api_java.business;
 
+import com.ayvy.api_java.infrastructure.entitys.Categoria;
+import com.ayvy.api_java.infrastructure.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoriaService {
+
+    private final CategoriaRepository repository;
+
+    public CategoriaService(CategoriaRepository repository) {this.repository = repository;}
+
+//CREATE
+    public void salvarCategoria(Categoria categoria){
+        repository.saveAndFlush(categoria);
+    }
+
+//READ
+    public Categoria buscarCategoriaNome_categoria(String nome_categoria){
+
+        return repository.findByNome_categoria(nome_categoria).orElseThrow(
+                //Uma exceção personalizada:
+                () -> new RuntimeException("Categoria não encontrada")
+        );
+    }
+
+//DELETE
+    public void deletarCategoriaPorId(Integer id) {
+        repository.deleteById(id);
+    }
+
+
+//UPDATE
+   // public void atualizarCategoriaPorId(Integer id, Categoria categoria){
+      //  Categoria categoriaRepository = repository.findById(id)
+  //  }
+
 }
--- src/main/java/com/ayvy/api_java/infrastructure/repository/CategoriaRepository.java
+++ src/main/java/com/ayvy/api_java/infrastructure/repository/CategoriaRepository.java
@@ -1,8 +1,13 @@
 package com.ayvy.api_java.infrastructure.repository;
 
 import com.ayvy.api_java.infrastructure.entitys.Categoria;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+    Optional<Categoria> findByNome_categoria(String nome_categoria);
 
 }
